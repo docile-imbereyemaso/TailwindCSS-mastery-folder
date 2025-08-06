27cca5e0f7436b5958a93f3f2c889168f8ecb74d
Docile123 <imbereyemasoo@gmail.com>
2025-08-06 17:47:49 +0200

    :feat:Adding blur background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Background from "./components/Background"
 
 
 function App() {
 
   return (
    <>
-   <h1 className="text-center text-amber-500">Hello world!</h1>
+  <Background/>
    </>
   )
 }
---
+++ src/components/Background.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <div className='min-h-screen grid items-center justify-center bg-black'>
+          <div className='relative group'>
+               <div className='absolute  -inset-0.5 rounded-xl blur bg-gradient-to-r from-pink-500 to-purple-600 opacity-75 group-hover:opacity-100 transition duration-1000'></div>
+               <button className='relative divide-x divide-gray-800 text-white bg-black px-10 py-6 rounded-xl text-xl tracking-wide'>
+                <span className='pr-6'>Lab Realese 2020.9</span>
+                <span className='pl-6 group-hover:text-gray-900 font-bold transition duration-200'>See what's new</span>
+               </button>
+          </div>
+    </div>
+  )
+}
+
+export default Background
