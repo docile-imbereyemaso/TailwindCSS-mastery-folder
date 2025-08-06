3699c5453316c068fb9f0e8dc631288d646d67eb
Docile123 <imbereyemasoo@gmail.com>
2025-08-06 22:45:05 +0200

    feat:revising the glowing background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Background = () => {
   return (
+  
     <div className='min-h-screen grid items-center justify-center bg-black'>
           <div className='relative group'>
                <div className='absolute  -inset-1 rounded-xl blur bg-gradient-to-r from-pink-500 to-purple-600 opacity-40 group-hover:opacity-100 transition duration-1000'></div>
@@ -10,6 +11,21 @@ const Background = () => {
                 <span className='pl-6 group-hover:text-gray-900 font-bold transition duration-200'>See what's new</span>
                </button>
           </div>
+
+
+          <div className='mt-5'>
+                  <div className='relative group'>
+                         <div className=' absolute -inset-0.5 blur bg-gradient-to-r from-pink-500 to-purple-500 opacity-30 group-hover:opacity-90 transition group-hover:duration-300 duration-1000'></div>
+                                  <button className=' relative px-3 py-5 bg-purple-400  text-white w-full rounded-xl divide-x divide-gray-900 text-xl tracking-widest'>
+                                    <span className='pr-4 group-hover:text-gray-700 transition group-hover:duration-1000 font-bold'>Lab Realese 2020.9</span>
+                                    <span className='pl-3'>See what's new</span>
+                                  </button>
+                         
+                  </div>
+          </div>
+
+
+          
     </div>
   )
 }
